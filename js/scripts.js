81f03f7f8e17c2e00961faf8f7da5f7d29c842c5
DavidNganga <ngashiedavid@gmail.com>
2018-03-04 20:41:19 +0300

    modified index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,30 @@ function rollDice() {
 	var roll = Math.floor(Math.random() * 6) + 1;
 	if (roll != 1) {
 		total = total + roll;
-		var roll1 = document.getElementById('roll1');
-		var roll2 = document.getElementById('roll2');
-		var status = document.getElementById('status');
-		var status2 = document.getElementById('status');
-		var diceTotal = roll1 + roll2;
+
+
+		//var roll1 = document.getElementById('roll1');
+		//var roll2 = document.getElementById('roll2');
+		///////var status = document.getElementById('status');
+		//var status2 = document.getElementById('status');
+		//var diceTotal = roll1 + roll2;
 		roll1.innerhtml = roll1
 		roll2.innerhtml = roll2
 	}
-	return .total;
+	return total;
+
 }
 $(document).ready(function() {
-	$("#roll1").click(function(e) {
-		e.preventDefault();
-		console.log("roll1");
-		$("#die1").text(rollDice());
+			$("#roll1").click(function(e) {
+				e.preventDefault();
+				console.log("roll1");
+				$(".result").text(rollDice());
 
-	});
-});
+				$("#roll2").click(function(e) {
+					e.preventDefault();
+					console.log("roll1");
+					$(".Result").text(rollDice());
+
+
+				});
+			})
